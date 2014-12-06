729356f63e353bc4a8198bd29b50daf4e0c0621d
Jeniean Las Pobres <jeniean@onvolo.com>
2014-12-06 16:46:44 +0800

    Initial ViewControllers

---
+++ BantayCiudad/Controllers/DetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.h
+//  BantayCiudad
+//
+//  Created by Jeniean Las Pobres on 12/6/14.
+//  Copyright (c) 2014 Bantay Ciudad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+
+@end
+
