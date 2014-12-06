8708175bfe8f459732c5c757bc1a89c34439adf0
Patrick Joseph Gorospe <patrick.gorospe@gmail.com>
2014-12-06 14:38:20 +0800

    Initial

---
+++ BantayCiudad/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  BantayCiudad
+//
+//  Created by Patrick Gorospe on 12/6/14.
+//  Copyright (c) 2014 Bantay Ciudad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ BantayCiudad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BantayCiudad
+//
+//  Created by Patrick Gorospe on 12/6/14.
+//  Copyright (c) 2014 Bantay Ciudad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
